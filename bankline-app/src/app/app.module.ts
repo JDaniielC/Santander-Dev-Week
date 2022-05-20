06aedd1520e4798105f208ed2a6243363187e82a
José Daniel Silva do Carmo <jdsc2@cin.ufpe.br>
2022-05-20 17:08:52 -0300

    Aula Angular finalizada
    Foram criadas 3 páginas, porém, não utilizei o bootstrap para o css, está faltando isso.

--- bankline-app/src/app/app.module.ts
+++ bankline-app/src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
+import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
+import { CorrentistaComponent } from './components/correntista/correntista.component';
+
+// Aqui registra a nossa localização.
+registerLocaleData(localePt);
+// Configurando o pipe (providers)
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovimentacaoListComponent,
+    MovimentacaoNewComponent,
+    CorrentistaComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    CurrencyMaskModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ bankline-app/src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
+
+@Component({
+  selector: 'app-movimentacao-list',
+  templateUrl: './movimentacao-list.component.html',
+  styleUrls: ['./movimentacao-list.component.scss'],
+})
+export class MovimentacaoListComponent implements OnInit {
+  movimentacoes: any;
+  constructor(private movimentacaoService: MovimentacaoService) {}
+
+  ngOnInit(): void {
+    this.listMovimentacoes();
+  }
+
+  listMovimentacoes(): void {
+    this.movimentacaoService.list().subscribe((data) => {
+      this.movimentacoes = data;
+    });
+  }
+}
